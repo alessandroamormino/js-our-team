6df9560cddd789c3a7badafddb8891088511dbcf
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-08 13:33:15 +0100

    feat: add project to do

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// CONSEGNA: 
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+
+// PSEUDOCODICE: 
+
+/*
+- creo un array di oggetti contentenenti le informazioni del team
+PER OGNI MEMBRO DEL TEAM
+    - stampo in console le sue informazioni
+    - BONUS: stampo nel DOM le sue informazioni in contenitori che stilizzerò dal CSS
+    BONUS: 
+    - prelevo la stringa contenente il nome dell'immagine e la imposto come src delle immagini nel DOM
+*/
+
+
